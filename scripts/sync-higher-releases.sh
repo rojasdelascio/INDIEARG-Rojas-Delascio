1dc86b837f6a9fba8c94ab273f5ac1d595f22d82
gus <rojasdelascio>
2025-10-07 02:26:34 +0000

    sznching with 3

--- scripts/sync-higher-releases.sh
+++ scripts/sync-higher-releases.sh
@@ -9,10 +9,14 @@ branches=($(git branch -r | grep 'origin/Release' | sed 's|origin/||'))
 
 for branch in "${branches[@]}"; do 
 
-    echo "currently on branch: $branch"
+    echo "currently on loop branch: $branch"
 
     MINOR_RELEASE_BRANCH="$(printf '%s\n' "$CURRENT_BRANCH" "$branch" | sort -V | head -n1)"
     echo "minor release: $MINOR_RELEASE_BRANCH"
+    # if [[ $branch == $CURRENT_BRANCH ]]; then
+    #     echo "Branch of this loop is the same as my current branch. Skipping..."
+    #     continue
+    # fi
     if [[ $MINOR_RELEASE_BRANCH == $CURRENT_BRANCH ]]; then
 
         echo "Current branch: $CURRENT_BRANCH is lower than branch: $branch. $CURRENT_BRANCH will be synched with $branch"
